1412864038d610c3ff71add67423b1dab0e791f1
Jirasak Kammakorn <jirasak.ka.54@ubu.ac.th>
2016-12-12 13:38:11 +0700

    no message

---
+++ src/java/model/Event.java
@@ -0,0 +1,41 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package model;
+
+/**
+ *
+ * @author jirasak_ka
+ */
+public class Event {
+    private String[] events;
+//    {
+//  "events": [
+//    {
+//      "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+//      "type": "message",
+//      "timestamp": 1462629479859,
+//      "source": {
+//        "type": "user",
+//        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+//      },
+//      "message": {
+//        "id": "325708",
+//        "type": "text",
+//        "text": "Hello, world"
+//      }
+//    },
+//    {
+//      "replyToken": "nHuyWiB7yP5Zw52FIkcQobQuGDXCTA",
+//      "type": "follow",
+//      "timestamp": 1462629479859,
+//      "source": {
+//        "type": "user",
+//        "userId": "U206d25c2ea6bd87c17655609a1c37cb8"
+//      }
+//    }
+//  ]
+//}
+}
