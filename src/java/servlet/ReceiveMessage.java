29f90e561d1724333ee071a63ee9dd56c553d692
Jirasak Kammakorn <jirasak.ka.54@ubu.ac.th>
2016-12-13 14:14:59 +0700

    fix

--- src/java/servlet/ReceiveMessage.java
+++ src/java/servlet/ReceiveMessage.java
@@ -55,63 +55,65 @@ public class ReceiveMessage extends HttpServlet {
             throws ServletException, IOException, java.security.InvalidKeyException {
         response.setContentType("text/html;charset=UTF-8");
 
-        // 署名検証
-        String body = null;
-        try (Stream<String> stream = request.getReader().lines()) {
-            String signature = request.getHeader("X-Line-Signature");
-            body = stream.reduce((s1, s2) -> s1 + "\n" + s2).orElse("");
-            SecretKeySpec key = new SecretKeySpec(CHANNEL_SECRET.getBytes(), HMAC_SHA256);
-            Mac mac = Mac.getInstance(HMAC_SHA256);
-            mac.init(key);
-            byte[] source = body.getBytes(StandardCharsets.UTF_8);
-            String createdSignature = Base64.encodeBase64String(mac.doFinal(source));
-            if (!signature.equals(createdSignature)) {
-                // LINEからのリクエストじゃない場合の処理
-                // 常に200を返す
-                response.setStatus(200);
-                return;
-            }
-        } catch (IOException e) {
-        } catch (NoSuchAlgorithmException e) {
-        } catch (InvalidKeyException e) {
-        }
-
-        ObjectMapper mapper = new ObjectMapper();
-        JsonNode events = mapper.readTree(body).path("events");
+        System.out.println("TEST HEROKU");
+        
+//        // 署名検証
+//        String body = null;
+//        try (Stream<String> stream = request.getReader().lines()) {
+//            String signature = request.getHeader("X-Line-Signature");
+//            body = stream.reduce((s1, s2) -> s1 + "\n" + s2).orElse("");
+//            SecretKeySpec key = new SecretKeySpec(CHANNEL_SECRET.getBytes(), HMAC_SHA256);
+//            Mac mac = Mac.getInstance(HMAC_SHA256);
+//            mac.init(key);
+//            byte[] source = body.getBytes(StandardCharsets.UTF_8);
+//            String createdSignature = Base64.encodeBase64String(mac.doFinal(source));
+//            if (!signature.equals(createdSignature)) {
+//                // LINEからのリクエストじゃない場合の処理
+//                // 常に200を返す
+//                response.setStatus(200);
+//                return;
+//            }
+//        } catch (IOException e) {
+//        } catch (NoSuchAlgorithmException e) {
+//        } catch (InvalidKeyException e) {
+//        }
+//
+//        ObjectMapper mapper = new ObjectMapper();
+//        JsonNode events = mapper.readTree(body).path("events");
+////        String type = events.path(0).path("message").path("type").asText();
+//        // リプライの種類（今回はtext）
+////        String type = events.path(0).path("message").path("type").asText(null);
 //        String type = events.path(0).path("message").path("type").asText();
-        // リプライの種類（今回はtext）
-//        String type = events.path(0).path("message").path("type").asText(null);
-        String type = events.path(0).path("message").path("type").asText();
-        // テキスト
-//        String query = events.path(0).path("message").path("text").asText(null);
-        String query = events.path(0).path("message").path("text").asText();
-        // 返信用Token
-//        String replyToken = events.path(0).path("replyToken").asText(null);
-        String replyToken = events.path(0).path("replyToken").asText();
-        // リプライを送る
-        HttpPost httpPost = new HttpPost("https://api.line.me/v2/bot/message/reply");
-        httpPost.setHeader("Content-Type", "application/json");
-        httpPost.setHeader("Authorization", "Bearer " + CHANNEL_ACCESS_TOKEN);
-        // 返信用のJSON
-        // サンプルとして「リプライありがとう！」を返す
-        String replyBody = String.format("{\"replyToken\":\"%s\", \"messages\":[{\"type\":\"text\", \"text\":\"リプライありがとう！\"}]}", replyToken);
-        StringEntity params = new StringEntity(replyBody, StandardCharsets.UTF_8);
-        httpPost.setEntity(params);
-        try (CloseableHttpClient client = HttpClients.createDefault();
-                CloseableHttpResponse resp = client.execute(httpPost);
-                BufferedReader br = new BufferedReader(new InputStreamReader(resp.getEntity().getContent(), StandardCharsets.UTF_8))) {
-            int statusCode = resp.getStatusLine().getStatusCode();
-            switch (statusCode) {
-                case 200:
-                    // ↓これは空のJSON({})が返るはず
-                    br.readLine();
-                    break;
-                default:
-            }
-        } catch (final ClientProtocolException e) {
-        } catch (final IOException e) {
-        }
-        response.setStatus(200);
+//        // テキスト
+////        String query = events.path(0).path("message").path("text").asText(null);
+//        String query = events.path(0).path("message").path("text").asText();
+//        // 返信用Token
+////        String replyToken = events.path(0).path("replyToken").asText(null);
+//        String replyToken = events.path(0).path("replyToken").asText();
+//        // リプライを送る
+//        HttpPost httpPost = new HttpPost("https://api.line.me/v2/bot/message/reply");
+//        httpPost.setHeader("Content-Type", "application/json");
+//        httpPost.setHeader("Authorization", "Bearer " + CHANNEL_ACCESS_TOKEN);
+//        // 返信用のJSON
+//        // サンプルとして「リプライありがとう！」を返す
+//        String replyBody = String.format("{\"replyToken\":\"%s\", \"messages\":[{\"type\":\"text\", \"text\":\"リプライありがとう！\"}]}", replyToken);
+//        StringEntity params = new StringEntity(replyBody, StandardCharsets.UTF_8);
+//        httpPost.setEntity(params);
+//        try (CloseableHttpClient client = HttpClients.createDefault();
+//                CloseableHttpResponse resp = client.execute(httpPost);
+//                BufferedReader br = new BufferedReader(new InputStreamReader(resp.getEntity().getContent(), StandardCharsets.UTF_8))) {
+//            int statusCode = resp.getStatusLine().getStatusCode();
+//            switch (statusCode) {
+//                case 200:
+//                    // ↓これは空のJSON({})が返るはず
+//                    br.readLine();
+//                    break;
+//                default:
+//            }
+//        } catch (final ClientProtocolException e) {
+//        } catch (final IOException e) {
+//        }
+//        response.setStatus(200);
 
     }
 
